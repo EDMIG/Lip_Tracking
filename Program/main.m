69f2d538284d6b8b068c18e06e664c5446f063b3
Sebkhi, Nordine <sebnor31@gatech.edu>
2016-03-18 18:37:46 -0400

    Created a main function to track lips on each frame

---
+++ Program/main.m
@@ -0,0 +1,23 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% This program tracks the movements of the lips
+% It is composed of a 2-step process:
+%
+%   1 - Identify the lips from RGB images
+%
+%   2 - Locate points on the lips to plot a line
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+close all;
+
+% Load a video
+videoFile = strcat(currentFolder, '\..\Data\allo_video.avi');
+video = VideoReader(videoFile);
+
+% currentFolder = pwd;
+% rawFrame = imread(strcat(currentFolder, '\..\Data\Snip20160318_3.png'));
+
+% Track lips in each frame
+while hasFrame(video)
+    rawFrame = readFrame(video,'native');
+    lipTracking(rawFrame);
+end
